24570a9e6856c1e23e08df23e2f840a34b59fef4
AbstractUltra <dragonhe36@gmail.com>
2020-04-26 00:52:15 +0100

    changes

--- components/FeedList.js
+++ components/FeedList.js
@@ -1,13 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import {View, StyleSheet, Text, SectionList} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, SectionList } from "react-native";
 import FeedListItem from "../components/FeedListItem";
 import FadeOverlay from "./FadeOverlay";
-import feed from '../data/feed';
-import { Divider } from 'react-native-elements';
+import feed from "../data/feed";
+import users from "../data/users";
+import { Divider } from "react-native-elements";
 
 export default function FeedList() {
   const [sectionedFeed, setSectionedFeed] = useState([]);
   const [fadeOpacity, setFadeOpacity] = useState(0);
+  const [userList, setUserList] = useState({});
 
   function handleScroll({ nativeEvent }) {
     if (nativeEvent.contentOffset.y <= 0) setFadeOpacity(0);
@@ -18,15 +20,17 @@ export default function FeedList() {
     setSectionedFeed([
       {
         title: "New Updates",
-        data: feed.filter((item) => item.new)
+        data: feed.filter((item) => item.new),
       },
       {
         title: "Recent Updates",
-        data: feed.filter((item) => !item.new)
-      }
-    ])
+        data: feed.filter((item) => !item.new),
+      },
+    ]);
+    for (let [key, value] of Object.entries(users)) {
+      setUserList({ ...userList, [value.handle]: value });
+    }
   }, []);
-
 
   return (
     <View style={styles.container}>
@@ -35,38 +39,43 @@ export default function FeedList() {
         sections={sectionedFeed}
         stickySectionHeadersEnabled={false}
         onScroll={handleScroll}
-        renderItem={({item, index, section}) => (
+        renderItem={({ item, index, section }) => (
           <FeedListItem
             name={item.name}
             handle={item.handle}
             content={item.content}
             challengeId={item.challengeId}
+            avatarUrl={
+              userList[item.handle] ? userList[item.handle].avatarUrl : null
+            }
             bottomDivider={index !== section.data.length - 1}
           />
         )}
-        renderSectionHeader={({section: { title } }) => (
-          <View style={{flex: -1}}>
-            <View style={{
-              flex: -1,
-              flexDirection: 'row',
-              alignItems: 'center',
-              backgroundColor: 'white'
-            }}>
-              <Divider style={{flexGrow: 1, marginHorizontal: 5}}/>
-              <Text style={{color: '#c1c8ce'}}>{title}</Text>
-              <Divider style={{flexGrow: 1, marginHorizontal: 5}}/>
+        renderSectionHeader={({ section: { title } }) => (
+          <View style={{ flex: -1 }}>
+            <View
+              style={{
+                flex: -1,
+                flexDirection: "row",
+                alignItems: "center",
+                backgroundColor: "white",
+              }}
+            >
+              <Divider style={{ flexGrow: 1, marginHorizontal: 5 }} />
+              <Text style={{ color: "#c1c8ce" }}>{title}</Text>
+              <Divider style={{ flexGrow: 1, marginHorizontal: 5 }} />
             </View>
           </View>
         )}
         keyExtractor={(item, index) => index}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    position: 'relative'
-  }
+    position: "relative",
+  },
 });
