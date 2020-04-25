65751abd5b45142105a6d4e816bc62f53c309588
AbstractUltra <dragonhe36@gmail.com>
2020-04-25 20:26:20 +0100

    changes

--- screens/ChallengesActive.js
+++ screens/ChallengesActive.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {FlatList, StyleSheet, View} from 'react-native';
 import user from "../data/user";
 import challenges from "../data/challenges";
 import ChallengeListItem from "../components/ChallengeListItem";
+import UserContext from "../contexts/user";
 
 export default function ChallengesActive() {
+  const {
+    user: { challenges: userChallenges },
+  } = useContext(UserContext);
+  console.log(userChallenges)
   return (
     <View style={styles.container}>
       <FlatList
-        data={Object.entries(user.challenges)}
+        data={Object.entries(userChallenges)}
         renderItem={({item: [itemId]}) => {
           const item = challenges[itemId];
           return <ChallengeListItem
