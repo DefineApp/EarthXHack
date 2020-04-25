5316a0d6552a7f06f8a0e07bbf25e290c417554a
AbstractUltra <dragonhe36@gmail.com>
2020-04-25 16:39:34 +0100

    changes

--- data/challenges.js
+++ data/challenges.js
@@ -1,52 +1,73 @@
 export default {
   1: {
     id: 1,
-    name: 'Earth Day 2020',
+    name: "Earth Day 2020",
     description: "Let's show Planet Earth that we care!",
-    startDate: new Date('2020-04-22'),
-    endDate: new Date('2020-04-29'),
-    tags: ['environment', 'earth day', 'green'],
-    logoUrl: '',
-    type: 'environment',
+    startDate: new Date("2020-04-22"),
+    endDate: new Date("2020-04-29"),
+    tags: ["environment", "earth day", "green"],
+    logoUrl: "",
+    type: "environment",
     totalTasks: 10,
     tasks: [
       {
-        name: 'Pick up 50 pieces of trash from the floor.',
-        description: "Submit a time-lapse video of you going around your neighbourhood and picking up 50 pieces of trash and throwing them in the garbage, where they belong! Make sure to take proper measures with trash on the ground: use latex gloves and make sure to thoroughly wash your hands after you're done!"
-      }
-    ]
+        name: "Pick up 50 pieces of trash from the floor.",
+        description:
+          "Submit a time-lapse video of you going around your neighbourhood and picking up 50 pieces of trash and throwing them in the garbage, where they belong! Make sure to take proper measures with trash on the ground: use latex gloves and make sure to thoroughly wash your hands after you're done!",
+      },
+      {
+        name: "Use reusable bags",
+        description:
+          "Shop with reusable bags! Take a picture of yourself shopping with reusable bags at the store! Make sure that your photo contains your username, so we'll know it's you!",
+      },
+      {
+        name: "Go outside",
+        description:
+          "Enjoy the outdoors! Earth Day is about celebrating our planet, and you can show your appreciation for her by sending us a photo of you enjoying the outside, whether that would be reading a book outside or having a picnic! In the end, judges will rank these submissions that will help determine the final rankings, so make them creative!",
+      },
+      {
+        name: "Shop at a local market",
+        description:
+          "Food in the grocery stores travels an average of 1,500 miles to get to you. Insetad, shopping at local farmer's market supports local businesses and is much better for the environment. For this task, you'll need to take pictures of yourself shopping at the local farmer's market!",
+      },
+      {
+        name: "Stop drinking oil",
+        description:
+          "It takes over 1.5 million barrels of oil to manufacture the amount of plastic water bottles we use up each year. It's a well-known fact that plastic bottles aren't great for the environment. We'd like you to take a picture of you drinking from your reusable water bottle, and a little description/background of how you met your reusable water bottle and how it has served you thus far.",
+      },
+    ],
   },
   2: {
     id: 2,
-    name: 'Energy Savers',
+    name: "Energy Savers",
     description: "Sustainable consumption starts with saving energy.",
-    startDate: new Date('2020-04-21'),
-    endDate: new Date('2020-04-23'),
-    tags: ['energy', 'efficiency'],
-    logoUrl: '',
-    type: 'energy',
-    totalTasks: 14
+    startDate: new Date("2020-04-21"),
+    endDate: new Date("2020-04-23"),
+    tags: ["energy", "efficiency"],
+    logoUrl: "",
+    type: "energy",
+    totalTasks: 14,
   },
   3: {
     id: 3,
-    name: 'Water Watchers',
+    name: "Water Watchers",
     description: "Saving water, one tap at a time.",
-    startDate: new Date('2020-04-23'),
-    endDate: new Date('2020-04-26'),
-    tags: ['water', 'efficiency'],
-    logoUrl: '',
-    type: 'water',
-    totalTasks: 13
+    startDate: new Date("2020-04-23"),
+    endDate: new Date("2020-04-26"),
+    tags: ["water", "efficiency"],
+    logoUrl: "",
+    type: "water",
+    totalTasks: 13,
   },
   4: {
     id: 4,
-    name: 'The 2nd R: Reuse',
+    name: "The 2nd R: Reuse",
     description: "Utilizing creativity to help keep waste out of landfills.",
-    startDate: new Date('2020-04-23'),
-    endDate: new Date('2020-04-27'),
-    tags: ['recycling', 'efficiency'],
-    logoUrl: '',
-    type: 'recycling',
-    totalTasks: 15
-  }
+    startDate: new Date("2020-04-23"),
+    endDate: new Date("2020-04-27"),
+    tags: ["recycling", "efficiency"],
+    logoUrl: "",
+    type: "recycling",
+    totalTasks: 15,
+  },
 };
