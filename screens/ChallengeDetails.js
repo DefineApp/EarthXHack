798ce6e18c8ba01c62fcb2235610de75d3ae549d
AbstractUltra <dragonhe36@gmail.com>
2020-04-25 16:54:23 +0100

    changes

--- screens/ChallengeDetails.js
+++ screens/ChallengeDetails.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import {Button} from "react-native-paper"
-import dateFormat from "dateformat"
+import { Button, Surface } from "react-native-paper";
+import dateFormat from "dateformat";
+import challenges from "../data/challenges";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function ChallengeDetails({ route, navigation }) {
   const {
@@ -14,18 +16,36 @@ export default function ChallengeDetails({ route, navigation }) {
     logoUrl,
     type,
   } = route.params;
-
+  const tasks = challenges[id].tasks;
   return (
     <View style={styles.container}>
       <View style={styles.basicInfo}>
         <Text style={{ fontWeight: "bold", fontSize: 25 }}>{name}</Text>
         <Text>
-          {dateFormat(startDate, "dd/mm/yyyy")} - {dateFormat(endDate, "dd/mm/yyyy")}
+          {dateFormat(startDate, "dd/mm/yyyy")} -{" "}
+          {dateFormat(endDate, "dd/mm/yyyy")}
         </Text>
         <Text>{description}</Text>
       </View>
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => (
+          <Surface>
+            <Text>{item.name}</Text>
+            <Text>{item.description}</Text>
+          </Surface>
+        )}
+        keyExtractor={(item) => item.name + item.description}
+      />
       <View style={styles.join}>
-        <Button mode="contained" icon="walk" onPress={()=>{}} style={{height: 50, justifyContent: 'center'}}>Join!</Button>
+        <Button
+          mode="contained"
+          icon="walk"
+          onPress={() => {}}
+          style={{ height: 50, justifyContent: "center" }}
+        >
+          Join!
+        </Button>
       </View>
     </View>
   );
@@ -41,6 +61,6 @@ const styles = StyleSheet.create({
   },
   join: {
     flex: 1,
-    justifyContent: "flex-end"
-  }
+    justifyContent: "flex-end",
+  },
 });
