e057d3acd7fc4a108b2f5d0f622271101d455f27
AbstractUltra <dragonhe36@gmail.com>
2020-04-25 18:11:25 +0100

    changes

--- screens/ChallengeDetails.js
+++ screens/ChallengeDetails.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import ChallengeDetailsJoinButton
-  from "../components/ChallengeDetailsJoinButton";
+import ChallengeDetailsJoinButton from "../components/ChallengeDetailsJoinButton";
 import { Button, Surface, List } from "react-native-paper";
-import { ListItem } from "react-native-elements";
+import { ListItem, Overlay } from "react-native-elements";
 import dateFormat from "dateformat";
 import challenges from "../data/challenges";
 import { FlatList } from "react-native-gesture-handler";
@@ -22,32 +21,58 @@ export default function ChallengeDetails({ route, navigation }) {
   const tasks = challenges[id].tasks;
   const [checkedTasks, setCheckedTasks] = useState({});
   function toggleTaskCheck(key) {
-    setCheckedTasks({...checkedTasks, [key]: !checkedTasks[key]});
+    setCheckedTasks({ ...checkedTasks, [key]: !checkedTasks[key] });
   }
+  const [overlayVisiblity, setOverlayVisibility] = useState(false);
+  const [overlayData, setOverlayData] = useState({});
   return (
     <View style={styles.container}>
       <View style={styles.basicInfo}>
         <Text style={{ fontWeight: "bold", fontSize: 25 }}>{name}</Text>
         <View style={styles.dateContainer}>
           <View style={styles.textContainer}>
-            <Text style={{fontWeight: 'bold'}}>Starts: </Text>
+            <Text style={{ fontWeight: "bold" }}>Starts: </Text>
             <Text>{dateFormat(startDate, "mmmm dS, yyyy, h:MM TT")}</Text>
           </View>
           <View style={styles.textContainer}>
-            <Text style={{fontWeight: 'bold'}}>Ends: </Text>
+            <Text style={{ fontWeight: "bold" }}>Ends: </Text>
             <Text>{dateFormat(endDate, "mmmm dS, yyyy, h:MM TT")}</Text>
           </View>
         </View>
         <Text>{description}</Text>
       </View>
       <View style={styles.tasks}>
+        <Overlay
+          isVisible={overlayVisiblity}
+          onBackdropPress={() => setOverlayVisibility(false)}
+          height="auto"
+        >
+          <View style={styles.overlayStyle}>
+            <View>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+                {overlayData.title}
+              </Text>
+            </View>
+            <Text style={{ paddingTop: 10 }}>{overlayData.description}</Text>
+          </View>
+        </Overlay>
         <FlatList
           data={tasks}
           renderItem={({ item, index }) => (
             <ListItem
               title={item.name}
               subtitle={item.description}
-              checkBox={{ checked: checkedTasks[index], onPress: () => toggleTaskCheck(index) }}
+              onPress={() => {
+                setOverlayVisibility(true);
+                setOverlayData({
+                  title: item.name,
+                  description: item.description,
+                });
+              }}
+              checkBox={{
+                checked: checkedTasks[index],
+                onPress: () => toggleTaskCheck(index),
+              }}
             />
           )}
           keyExtractor={(item, index) => index.toString()}
@@ -68,14 +93,17 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   tasks: {
-    flex: 1
+    flex: 1,
   },
   textContainer: {
     flex: -1,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   dateContainer: {
     flex: -1,
     margin: 5,
-  }
+  },
+  overlayStyle: {
+    margin: 10,
+  },
 });
