73977470bcd6e8931eefb9fb2583e7c03a561da2
AbstractUltra <dragonhe36@gmail.com>
2020-04-25 15:38:47 +0100

    changes

--- components/ChallengeListItem.js
+++ components/ChallengeListItem.js
@@ -8,7 +8,7 @@ import ChallengeGradients from "../constants/ChallengeColors";
 import ChallengeContext from "../contexts/challenge";
 import ChallengeListItemProgressCircle from "./ChallengeListItemProgressCircle";
 import ChallengeListItemProgressBar from "./ChallengeListItemProgressBar";
-import navigationRef from "../navigation/RootNavigation";
+import {useNavigation} from "@react-navigation/native"
 
 function ChallengesSearchListItemContent() {
   const {description, startDate, endDate, tags, showProgressBar} = useContext(ChallengeContext);
@@ -65,7 +65,7 @@ function ChallengesSearchListItemContent() {
 */
 
 export default function ChallengesSearchListItem(props) {
-  const navigation = navigationRef.current;
+  const navigation = useNavigation();
   return (
     <ChallengeContext.Provider value={{...props}}>
       <ListItem
