2180d88d891a7b17810eabff16e8d72e165858bb
Dragon He <dragonhe36@gmail.com>
2020-04-26 19:25:56 +0100

    changes

--- components/ProfileScreenFollowButton.js
+++ components/ProfileScreenFollowButton.js
@@ -1,21 +1,40 @@
 import React, { useContext } from "react";
 import { View } from "react-native";
 import { Button } from "react-native-elements";
-import LoggedInUserContext from "../contexts/LoggedInUser"
-import UserContext from "../contexts/User"
+import LoggedInUserContext from "../contexts/LoggedInUser";
+import UserContext from "../contexts/User";
 import usePatchData from "../hooks/usePatchData";
 
 export default function ProfileScreenFollowButton() {
-  const { user: loggedInUser } = useContext(LoggedInUserContext);
-  const {user} = useContext(UserContext)
+  const { user: loggedInUser, setUser: setLoggedInUser } = useContext(
+    LoggedInUserContext
+  );
+  const { user } = useContext(UserContext);
   const patchUser = usePatchData(`users/${loggedInUser.id}`);
 
   return (
-    <View style={{padding:30}}>
-      <Button title="Follow" onPress={async () => {
-        loggedInUser.following.push(user.id);
-        await patchUser({following: loggedInUser.following});
-      }} />
+    <View style={{ padding: 30 }}>
+      {!loggedInUser.following.includes(user.id) ? (
+        <Button
+          title="Follow"
+          onPress={async () => {
+            await setLoggedInUser({
+              ...loggedInUser,
+              ['following']: loggedInUser.following.concat(user.id),
+            });
+            await patchUser({ following: loggedInUser.following });
+          }}
+        />
+      ) : (
+        <Button
+          title="Unfollow"
+          onPress={async () => {
+            const targetIndex = loggedInUser.following.indexOf(user.id);
+            loggedInUser.following.splice(targetIndex, 1);
+            await patchUser({ following: loggedInUser.following });
+          }}
+        />
+      )}
     </View>
   );
 }
