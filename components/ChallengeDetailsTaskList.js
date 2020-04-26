eacc66735ffbc2e2de8b4fbbe95a748b8434ddc4
Dragon He <dragonhe36@gmail.com>
2020-04-26 04:27:07 +0100

    changes

--- components/ChallengeDetailsTaskList.js
+++ components/ChallengeDetailsTaskList.js
@@ -1,13 +1,24 @@
-import React, {useContext} from "react";
-import {FlatList, StyleSheet, Text, View} from "react-native";
+import React, { useContext } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import UserContext from "../contexts/user";
 import challenges from "../data/challenges";
 import ChallengeContext from "../contexts/challenge";
 import ChallengeDetailsTaskListItem from "./ChallengeDetailsTaskListItem";
 
 export default function ChallengeDetailsTaskList() {
   const { id: challengeId } = useContext(ChallengeContext);
+  const { user, setUser, user: { challenges: userChallenges } } = useContext(UserContext);
 
   const tasks = challenges[challengeId].tasks;
+  const completedTasks = userChallenges[challengeId].checkedTasks;
+
+  if (tasks && completedTasks) {
+    if (
+      Object.entries(tasks).length === Object.entries(completedTasks).length
+    ) {
+      alert("All tasks have been completed!");
+    }
+  }
 
   return (
     <View style={styles.tasks}>
@@ -50,4 +61,3 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
