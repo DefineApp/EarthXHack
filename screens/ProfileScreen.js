6b7406c0e8a4363dc16f68fe5886ac775450ea10
Leon Si <leonzalion@gmail.com>
2020-04-25 20:08:52 -0400

    changes

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -6,38 +6,18 @@ import challengeList from "../data/challenges";
 import ChallengeListItem from "../components/ChallengeListItem";
 import TouchableScale from "react-native-touchable-scale";
 
-export default function ProfileScreen() {
-  const [userChallenges, setUserChallenges] = useState([]);
-  const {
-    user: {
-      name,
-      handle,
-      followers,
-      following,
-      description,
-      challenges,
-      avatarUrl,
-    }
-  } = useContext(UserContext);
-
-  function retrieveChallengesForUser() {
-    let arr = [];
-    for (let [key, value] of Object.entries(challenges)) {
-      arr.push({
-        id: key,
-        tasksDone: value.tasksDone,
-        ...challengeList[key]
-      });
-    }
-    setUserChallenges(arr);
+function ProfileScreenUserInformation({
+  user: {
+    name,
+    handle,
+    followers,
+    following,
+    description,
+    avatarUrl,
   }
-
-  useEffect(() => {
-    retrieveChallengesForUser();
-  }, []);
-
+}) {
   return (
-    <View style={styles.profileSummary}>
+    <View style={styles.profileBasicsContainer}>
       <View style={styles.profileBasics}>
         <View>
           <Avatar rounded source={{ uri: avatarUrl }} size={125} />
@@ -74,26 +54,59 @@ export default function ProfileScreen() {
       <View style={{ alignItems: "center" }}>
         <Text style={{ fontWeight: "bold", fontSize: 20, marginTop: 10 }}>Active Challenges</Text>
       </View>
-      <FlatList
-        data={userChallenges}
-        renderItem={({ item }) => (
-          <ChallengeListItem
-            {...item}
-            showStartDate={false}
-          />
-        )}
-        keyExtractor={(item) => item.id.toString()}
-        scrollEnabled={false}
-      />
     </View>
+  );
+}
+
+export default function ProfileScreen() {
+  const [userChallenges, setUserChallenges] = useState([]);
+  const { user } = useContext(UserContext);
+
+  function retrieveChallengesForUser() {
+    let arr = [];
+    for (let [key, value] of Object.entries(user.challenges)) {
+      arr.push({
+        id: key,
+        tasksDone: value.tasksDone,
+        ...challengeList[key]
+      });
+    }
+    setUserChallenges(arr);
+  }
+
+  useEffect(() => {
+    retrieveChallengesForUser();
+  }, []);
+
+  return (
+    <FlatList
+      style={styles.profileSummary}
+      ListHeaderComponent={
+        <ProfileScreenUserInformation user={user} />
+      }
+      ListFooterComponent={
+        <View style={{marginBottom: 20}} />
+      }
+      data={userChallenges}
+      renderItem={({ item }) => (
+        <ChallengeListItem
+          {...item}
+          showStartDate={false}
+        />
+      )}
+      keyExtractor={(item) => item.id.toString()}
+      scrollEnabled={true}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   profileSummary: {
-    padding: 20,
     flex: 1,
     backgroundColor: "white",
+  },
+  profileBasicsContainer: {
+    marginTop: 20
   },
   profileBasics: {
     flexDirection: "row",
