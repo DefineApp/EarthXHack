8b51f9248d48fbfe392ad5039724e60a90bbe57f
Leon Si <leonzalion@gmail.com>
2020-04-26 10:50:59 -0400

    changes

--- hooks/usePutData.js
+++ hooks/usePutData.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import to from 'await-to-js';
 
-export default function usePostData(route) {
-  return async function postData(body) {
+export default function usePutData(route) {
+  return async function putData(body) {
     const url = `https://define-json-server.herokuapp.com/${route}`;
     const [error, { data }] = await to(axios.put(url, body));
     if (error) return alert(error);
